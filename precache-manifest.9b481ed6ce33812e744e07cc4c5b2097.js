85d7854ec40bc8de8662b0b53e3fe79cbd89b0da
MarikKV <kosaniakm@gmail.com>
2020-07-10 14:24:28 +0300

    Updates

---
+++ precache-manifest.9b481ed6ce33812e744e07cc4c5b2097.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "505595b6994727f6cf993b1956d519ca",
+    "url": "/Tasker/index.html"
+  },
+  {
+    "revision": "f5a22a2b0af35ccfbaf2",
+    "url": "/Tasker/static/css/main.95963bc7.chunk.css"
+  },
+  {
+    "revision": "1e12f5d10f27933aa173",
+    "url": "/Tasker/static/js/2.0dce80f1.chunk.js"
+  },
+  {
+    "revision": "544111c69119227bbf075a65771ccdb1",
+    "url": "/Tasker/static/js/2.0dce80f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5a22a2b0af35ccfbaf2",
+    "url": "/Tasker/static/js/main.400ec9ae.chunk.js"
+  },
+  {
+    "revision": "8a5e72e2daac0a773594",
+    "url": "/Tasker/static/js/runtime-main.b8c6a532.js"
+  }
+]);
