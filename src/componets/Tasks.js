4a300fe3b88bc22f2e7a0f7f3318e8fd88cf922e
MarikKV <kosaniakm@gmail.com>
2020-07-12 16:36:04 +0300

    add editing tasks

--- src/componets/Tasks.js
+++ src/componets/Tasks.js
@@ -2,14 +2,24 @@ import React, { useState, useEffect } from 'react';
 import AddNewTask from './AddNewTask';
 import { useSelector, useDispatch } from 'react-redux';
 import { getTasks } from '../actions';
-import { getCollectionWhere } from '../customFunctions';
+import { getCollectionWhere, editTaskFB } from '../customFunctions';
 import { db } from '../firebase';
 
-import { Button, Table } from 'react-bootstrap';
+import { Button, Table, Modal, Form } from 'react-bootstrap';
 import '../styles/Tasks.scss';
 
 
 export default function Tasks() {
+    //modal edit task
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    const [edit_id, setId] = useState('');
+    const [edit_title, setTitle] = useState('');
+    const [edit_describe, setDescribe] = useState('');
+    const [edit_status, setStatus] = useState('');
+
     const saveUserInStore = useSelector(state => state.saveUserInStore);
     const allUserTasks = useSelector(state => state.allUserTasks);
     const [showForm, setShowForn] = useState(false)
@@ -46,7 +56,12 @@ export default function Tasks() {
         .get()
         .then( doc => {
           if (doc.exists) {
-            console.log("Document data:", doc.data());
+            let taskData = doc.data()
+            setId(id)
+            setTitle(taskData.title)
+            setDescribe(taskData.describe)
+            setStatus(taskData.status)
+            handleShow();
           } else {
             console.log("No such document!");
           }
@@ -54,6 +69,13 @@ export default function Tasks() {
         .catch( err => {
           console.log("Error getting document:", err);
         });
+    }
+
+    const saveEditedTask = () => {
+        console.log('try to edit task',edit_id, edit_title, edit_describe, edit_status);
+        editTaskFB(edit_id, edit_title, edit_describe, edit_status);
+        gettodos()
+        handleClose()
     }
 
     const gettodos = () => {
@@ -83,7 +105,8 @@ export default function Tasks() {
                             <th>#</th>
                             <th>Title</th>
                             <th>Describe</th>
-                            <th>Edit task(in procces)</th>
+                            <th>Status</th>
+                            <th>Edit task</th>
                             <th>Delete task</th>
                         </tr>
                     </thead>
@@ -92,7 +115,12 @@ export default function Tasks() {
                             <tr key={index}>
                                 <td>{index+1}</td>
                                 <td>{item.title}</td>
-                                <td>{item.describe}</td>
+                                <td className='table_deskribe'>{item.describe}</td>
+
+                                {item.status === 'in progress' ? <td className='status_progress'><b>{item.status}</b></td> : null}
+                                {item.status === 'done' ? <td className='status_done'><b>{item.status}</b></td> : null}
+                                {item.status === 'failed' ? <td className='status_failed'><b>{item.status}</b></td> : null}
+
                                 <td><Button variant='warning' value={item.id} onClick={e => editTask(e.target.value)}>Edit</Button></td>
                                 <td><Button variant='danger' value={item.id} onClick={e => deleteTask(e.target.value)}>Delete</Button></td>
                             </tr>
@@ -105,6 +133,47 @@ export default function Tasks() {
                 <AddNewTask/>
             </div>
             <div className='togleForm' onClick={()=>togleAddTask(showForm)}>{showForm ? '<' : '>'}</div>
+
+            {/* Форма для редагування завдання*/}
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                <Modal.Title>Editing task</Modal.Title>
+                </Modal.Header>
+
+                <Modal.Body>
+                    <Form>
+                        <Form.Label>Title</Form.Label>
+                        <Form.Control   
+                            value={edit_title} 
+                            onChange={e => setTitle(e.target.value)}
+                        />
+
+                        <Form.Label>Describe</Form.Label>
+                        <Form.Control 
+                            as="textarea" rows="4"  
+                            value={edit_describe} 
+k                            onChange={e => setDescribe(e.target.value)}
+                        />
+
+                        <Form.Label>Status</Form.Label>
+                        <Form.Control as="select" value={edit_status} onChange={e => setStatus(e.target.value)}>
+                            <option>in progress</option>
+                            <option>done</option>
+                            <option>failed</option>
+                        </Form.Control>
+                        
+                    </Form>
+                </Modal.Body>
+
+                <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                    Close
+                </Button>
+                <Button variant="primary" onClick={saveEditedTask}>
+                    Save Changes
+                </Button>
+                </Modal.Footer>
+            </Modal>
         </div>
     )
 }
--- src/customFunctions.js
+++ src/customFunctions.js
@@ -38,13 +38,28 @@ export const addNewTaskToDB = (userId, title, describe) => {
     db.collection("todos").doc().set({
         userId: userId,
         title: title,
-        describe: describe
+        describe: describe,
+        status: 'in progress'
     })
     .then(function() {
         console.log("Task successfully added!");
     })
     .catch(function(error) {
         console.error("Error adding task: ", error);
+    });
+}
+
+export const editTaskFB = (id, title, describe, status) => {
+    db.collection("todos").doc(id).update({
+        title: title,
+        describe: describe,
+        status: status
+    })
+    .then(function() {
+        console.log("Task successfully edited!");
+    })
+    .catch(function(error) {
+        console.error("Error edit task: ", error);
     });
 }
 
